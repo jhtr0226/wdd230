14c95b6dafe6b6a626abb7d7cdee15950fbf9651
jhtr0226 <johanncraft17@gmail.com>
2024-01-18 23:53:12 -0600

    basics covered

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const year = new Date().getFullYear();
 
     const firstP = document.querySelector("footer p:first-of-type");
-    firstP.innerHTML = `&copy ${year} <br>
-    Johann Tellez Rodriguez <br>
+    firstP.innerHTML = `&copy ${year} 
+    Johann Tellez Rodriguez 
     Costa Rica <img id="flag" src = "images/costa-rica.png" alt="costarican Flag">`;
 
     const lastModifiedDate = document.lastModified;
