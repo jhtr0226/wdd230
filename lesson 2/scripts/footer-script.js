4f1cdb25b36a2a2632dece2176287214afdcee40
jhtr0226 <johanncraft17@gmail.com>
2024-01-19 23:33:50 -0600

    test

---
+++ lesson 2/scripts/footer-script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const year = new Date().getFullYear();
+
+    const firstP = document.querySelector("footer p:first-of-type");
+    firstP.innerHTML = `&copy ${year} 
+     | Johann Tellez Rodriguez | `;
+
+    const lastModifiedDate = document.lastModified;
+    const secondP = document.getElementById("lastModified");
+    secondP.textContent = `Last Modified: ${lastModifiedDate}`;
+});
