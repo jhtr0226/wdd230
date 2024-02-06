8eb336b54bb506de2b98308b7897606aa6cda094
jhtr0226 <johanncraft17@gmail.com>
2024-02-06 11:49:25 -0600

    almost perfect

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const year = new Date().getFullYear();
 
+    const firstP = document.querySelector("footer p:first-of-type");
+    firstP.innerHTML = `&copy ${year} 
+    Johann Tellez Rodriguez <br>WDD230 Web Frontend Development <br><br>info@example.com <br>+1 123 456 7890`;
+
     const lastModifiedDate = document.lastModified;
     const secondP = document.getElementById("lastModified");
     secondP.textContent = `Last Modified: ${lastModifiedDate}`;
