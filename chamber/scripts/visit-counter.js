228779998c820f89cb25aeeda9c4a8267bae2e4f
jhtr0226 <johanncraft17@gmail.com>
2024-02-25 03:16:17 -0600

    us

--- chamber/scripts/visit-counter.js
+++ chamber/scripts/visit-counter.js
@@ -28,5 +28,5 @@ localStorage.setItem('visitCount', visitCount.toString());
 
 const visitCountElement = document.querySelector('.visits-counter');
 if (visitCountElement) {
-    visitCountElement.textContent = `Visits: ${visitCount}`;
+    visitCountElement.textContent = `Times you visited us: ${visitCount}`;
 }
