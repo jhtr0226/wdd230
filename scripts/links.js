535f361e501a1384a2f909ae1070aaa4afd8d1d8
jhtr0226 <johanncraft17@gmail.com>
2024-03-07 23:34:22 -0600

    api

---
+++ scripts/links.js
@@ -0,0 +1,34 @@
+const baseURL = "https://jhtr0226.github.io/wdd230/";
+const linksURL = "https://jhtr0226.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+getLinks();
+function displayLinks(weeks) {
+    const linksList = document.querySelector('.my-list');
+
+    weeks.forEach(week => {
+        const WeekNum = week.lesson;
+        const links = week.links;
+
+        const weekListNum = document.createElement('li');
+        weekListNum.textContent = `${WeekNum}`;
+        const listOfLinks = document.createElement('ul');
+
+        links.forEach((link) => {
+            const linkListNum = document.createElement('li');
+            const linkA = document.createElement('a');
+
+            linkA.href = baseURL + linksURL;
+            linkA.textContent = link.title;
+            linkListNum.appendChild(linkA);
+            listOfLinks.appendChild(linkListNum);
+        });
+
+        linksList.appendChild(weekListNum);
+        linksList.appendChild(listOfLinks);
+    });
+}
