7c04bb14ca278ea734f2975ea7795864ffed0900
jhtr0226 <johanncraft17@gmail.com>
2024-03-09 21:13:31 -0600

    hj

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -24,7 +24,7 @@ const space = document.querySelector('#card1');
 async function getMembers() {
     const response = await fetch(members);
     const data = await response.json();
-
+    console.log(data.company);
     displayMembers(data.companies);
 }
 
@@ -37,15 +37,28 @@ const displayMembers = (companies) => {
         let phone = document.createElement('p');
         let website = document.createElement('a');
         let membership = document.createElement('p');
-        let address = document.createElement('p');
+        let address = document.createElement('a');
         let time = document.createElement('p');
         let images = document.createElement('img');
 
-        images.setAttribute('src', company.image);
+        name.textContent = company.info[0].name;
+        phone.textContent = `Phone: ${company.info[0].phone}`;
+        website.textContent = `Web`;
+        website.setAttribute('href', company.info[0].url);
+        website.setAttribute('target', '_blank');
+        membership.textContent = `Membership Level: ${company.info[0].membership
+            }`;
+        address.textContent = `Address: ${company.info[0].address}`;
+        address.setAttribute('href', `https://www.google.com/maps/place/${encodeURIComponent(company.info[0].address)}`);
+        address.setAttribute('target', '_blank');
+        time.textContent = `${company.info[0].loyalty
+            } as a member`;
+
+        images.setAttribute('src', company.info[0].image);
         images.setAttribute('alt', 'Image of commerce');
         images.setAttribute('loading', 'lazy');
-        images.setAttribute('width', '340');
-        images.setAttribute('height', '440');
+        images.setAttribute('width', '400');
+        images.setAttribute('height', '250');
 
         card.appendChild(name);
         card.appendChild(phone);
