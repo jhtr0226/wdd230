fa3a19bd8d35231ac65c0993b2bcd10f327622e2
jhtr0226 <johanncraft17@gmail.com>
2024-03-14 22:40:44 -0600

    gs

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,6 +2,11 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
+const time = new Date().getHours();
+
+const nightColor = '#4123aa';
+const dayColor = '#6f52d6';
+
 /*coordenates 9.946397494268446, -84.05296172378557*/
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=9.98&lon=-84.05&appid=bb5de5d3b5542a168c6a4a0d75756bd0&units=imperial&units=imperial";
 
@@ -30,4 +35,12 @@ function displayResults(data) {
     weatherIcon.setAttribute('src', iconsrc)
     weatherIcon.setAttribute('alt', desc)
     captionDesc.textContent = desc;
+}
+
+
+if (time >= 18 || time < 6) {
+    weatherIcon.style.backgroundColor = nightColor;
+}
+else {
+    weatherIcon.style.backgroundColor = dayColor;
 }
